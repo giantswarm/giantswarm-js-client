47a43d8262f45dddae5b3707095b93ed50e0ae47
marians <marian@giantswarm.io>
2015-06-15 15:13:48 +0200

    Whitespace

--- spec/client_spec.js
+++ spec/client_spec.js
@@ -1,27 +1,27 @@
 describe("Client", function() {
 
-    var GiantSwarm = require('../lib/client');
+  var GiantSwarm = require('../lib/client');
 
-    beforeEach(function() {
-      GiantSwarm.setApiEndpoint('https://api.giantswarm.io');
-      GiantSwarm.setAuthToken(null);
-    });
+  beforeEach(function() {
+    GiantSwarm.setApiEndpoint('https://api.giantswarm.io');
+    GiantSwarm.setAuthToken(null);
+  });
 
-    it("should allow me to ping the server", function(done){
-      GiantSwarm.ping(function(){
-        var value = 1;
-        expect(value).toEqual(1);
-        done();
-      });
+  it("should allow me to ping the right server", function(done){
+    GiantSwarm.ping(function(){
+      var value = 1;
+      expect(value).toEqual(1);
+      done();
     });
+  });
 
-    it("should not allow me to ping the server", function(done){
-      GiantSwarm.setApiEndpoint('https://www.google.com');
-      GiantSwarm.ping(function(){}, function(err){
-        var value = 1;
-        expect(value).toEqual(1);
-        done();
-      });
+  it("should not allow me to ping google.com", function(done){
+    GiantSwarm.setApiEndpoint('https://www.google.com');
+    GiantSwarm.ping(function(){}, function(err){
+      var value = 1;
+      expect(value).toEqual(1);
+      done();
     });
+  });
 
 });
