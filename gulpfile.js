42d85a7c3687b6c979adf6d3bfe6c08675c13844
Oliver Ponder <oliver.ponder@gmail.com>
2015-08-24 01:54:27 +0200

    Add eslint.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,14 @@ var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
+var eslint = require('gulp-eslint');
+
+gulp.task('eslint', function() {
+  return gulp.src('./lib/client.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError());
+});
 
 gulp.task('default', function() {
   // set up the browserify instance on a task basis
