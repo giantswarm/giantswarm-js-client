6851f5833962152bd0bcaaf54a3d42d3365fb931
Oliver Ponder <oliver.ponder@gmail.com>
2015-08-23 23:06:10 +0200

    Use definition, not config

--- spec/mock_api/v1/org_env_service_definition.js
+++ spec/mock_api/v1/org_env_service_definition.js
@@ -1,5 +1,5 @@
 var match = function match(match, params, headers) {
-  if (match[1] === '/v1/org/oponder/env/dev/service/known_service/config') {
+  if (match[1] === '/v1/org/oponder/env/dev/service/known_service/definition') {
     return {
       body: {
         "status_code": 10000,
